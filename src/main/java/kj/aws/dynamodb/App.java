3d7ca2bb6cedd23c14619c0ebe9e90614a554618
kj <kouji.wtnb@gmail.com>
2014-12-21 22:14:11 +0900

    jarにaws-sdkを含める

--- src/main/java/kj/aws/dynamodb/App.java
+++ src/main/java/kj/aws/dynamodb/App.java
@@ -1,13 +1,26 @@
 package kj.aws.dynamodb;
 
-/**
- * Hello world!
- *
+import com.amazonaws.auth.AWSCredentialsProviderChain;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.ListTablesRequest;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
+
+/*
+ * mvn exec:java -Dexec.mainClass=kj.aws.dynamodb.App
+ * もしくは mvn clean packageしてから
+ * java -jar ./target/how2aws_dynamodb-1.0.0-jar-with-dependencies.jar
+ * で実行する
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App{
+	public static void main( String[] args ){
+		AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+		client.setRegion(Region.getRegion(Regions.AP_NORTHEAST_1));
+		ListTablesRequest request = new ListTablesRequest();
+		ListTablesResult result = client.listTables(request);
+		for(String name : result.getTableNames()){
+			System.out.println(name);
+		}
+	}
 }
