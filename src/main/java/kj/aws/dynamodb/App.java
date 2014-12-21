73d14bfca94b9bfed76ee927457d8662e80dd240
kj <kouji.wtnb@gmail.com>
2014-12-21 21:16:01 +0900

    first commit

---
+++ src/main/java/kj/aws/dynamodb/App.java
@@ -0,0 +1,13 @@
+package kj.aws.dynamodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
